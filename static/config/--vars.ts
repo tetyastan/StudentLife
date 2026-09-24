1e71756e7a600b6d41a84248824ae54184a13b13
130414 <32320593+tetyastan@users.noreply.github.com>
2026-09-24 14:56:56 +0700

    Added some lore, removed unusable files.

--- static/config/--vars.ts
+++ static/config/--vars.ts
@@ -1,10 +1,5 @@
 import { Character } from '$lib/server/runtime_types.js';
 
-export const hero = new Character('Hero');
-export const narrator = new Character('Narrator');
-export const ghost = new Character('Ghost');
-
-export const test_scalar = 42;
-export const test_flag = false;
-export const test_list = [];
-export const test_dict = { a: 1, b: 2 };
+export const mc = new Character('Миша');
+export const st = new Character('Стёпа');
+export const tm = new Character('Тимур');
--- static/config/game_init.ts
+++
@@ -1,5 +0,0 @@
-import { Character } from '$lib/server/runtime_types.js';
-
-export const merchant = new Character('Merchant');
-export const starting_gold = 100;
-export const initial_key = false;
